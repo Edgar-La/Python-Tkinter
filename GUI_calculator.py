d95f0b0592071350b7f96f39e48323df35f0a301
Edgar-La <laraae2018@licifug.ugto.mx>
2020-07-04 22:09:01 -0500

    Do the basic operations (two bumbers at time).

--- GUI_calculator.py
+++ GUI_calculator.py
@@ -19,18 +19,49 @@ def dot_():
 	e.insert(END, '.')
 
 def sum_():
-	return
-def dot_():
-	return
+	global R_
+	R_ = float(e.get())
+	global Operation_
+	Operation_ = 'Sum'
+	e.delete(0, END)
+def sus_():
+	global R_
+	R_ = float(e.get())
+	global Operation_
+	Operation_ = 'Substraction'
+	e.delete(0, END)
+def mul_():
+	global R_
+	R_ = float(e.get())
+	global Operation_
+	Operation_ = 'Multiplication'
+	e.delete(0, END)
+def div_():
+	global R_
+	R_ = float(e.get())
+	global Operation_
+	Operation_ = 'Division'
+	e.delete(0, END)
+
 def eq_():
-	return
+	second = e.get()
+	e.delete(0, END)
+	if 	Operation_ == 'Sum':
+		e.insert(0, R_ + float(second))
+	elif 	Operation_ == 'Substraction':
+		e.insert(0, R_ - float(second))
+	elif 	Operation_ == 'Multiplication':
+		e.insert(0, R_ * float(second))
+	elif 	Operation_ == 'Division':
+		e.insert(0, R_ / float(second))
+
 def act_():
 	return
 
 button_l = Button(root, text = '(', padx = 35, pady = 35, bg = '#004D40', fg = 'white', command = lambda: act_()).grid(row = 1, column = 0)
 button_r = Button(root, text = ')', padx = 35, pady = 35, bg = '#004D40', fg = 'white', command = lambda: act_()).grid(row = 1, column = 1)
 button_C = Button(root, text = 'C', padx = 35, pady = 35, bg = '#004D40', fg = 'white', command = clear_).grid(row = 1, column = 2)
-button_div = Button(root, text = '%', padx = 34, pady = 35, bg = '#00796B', fg = 'white', command = lambda: act_()).grid(row = 1, column = 3)
+button_div = Button(root, text = '/', padx = 34, pady = 35, bg = '#00796B', fg = 'white', command = div_).grid(row = 1, column = 3)
 
 button_1 = Button(root, text = '1', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(1)).grid(row = 4, column = 0)
 button_2 = Button(root, text = '2', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(2)).grid(row = 4, column = 1)
@@ -40,12 +71,12 @@ button_sum = Button(root, text = '+', padx = 34, pady = 35, bg = '#00796B', fg =
 button_4 = Button(root, text = '4', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(4)).grid(row = 3, column = 0)
 button_5 = Button(root, text = '5', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(5)).grid(row = 3, column = 1)
 button_6 = Button(root, text = '6', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(6)).grid(row = 3, column = 2)
-button_sus = Button(root, text = '-', padx = 36.4, pady = 35, bg = '#00796B', fg = 'white', command = lambda: act_()).grid(row = 3, column = 3)
+button_sus = Button(root, text = '-', padx = 36.4, pady = 35, bg = '#00796B', fg = 'white', command = sus_).grid(row = 3, column = 3)
 
 button_7 = Button(root, text = '7', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(7)).grid(row = 2, column = 0)
 button_8 = Button(root, text = '8', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(8)).grid(row = 2, column = 1)
 button_9 = Button(root, text = '9', padx = 35, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(9)).grid(row = 2, column = 2)
-button_mul = Button(root, text = 'x', padx = 35, pady = 35, bg = '#00796B', fg = 'white', command = lambda: act_()).grid(row = 2, column = 3)
+button_mul = Button(root, text = 'x', padx = 35, pady = 35, bg = '#00796B', fg = 'white', command = mul_).grid(row = 2, column = 3)
 
 button_0 = Button(root, text = '0', padx = 79, pady = 35, bg = '#263238', fg = 'white', command = lambda: num_(0)).grid(row = 5, column = 0, columnspan = 2)
 butoon_dot = Button(root, text = '.', padx = 37, pady = 35,bg = '#263238', fg = 'white', command = dot_).grid(row = 5, column = 2)
