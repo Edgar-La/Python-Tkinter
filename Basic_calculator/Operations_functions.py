09415a1454c4d7a1663ecab6b87961148c53f934
Edgar-La <laraae2018@licifug.ugto.mx>
2020-07-05 21:30:17 -0500

    Separated: main file and script file

--- Basic_calculator/Operations_functions.py
+++ Basic_calculator/Operations_functions.py
@@ -1,57 +1,57 @@
-from tkinter import *
+#from tkinter import *
+from tkinter import END
 
-root = Tk()
-root.title('Simple calculator')
-root.configure(background='#263238')
-e = Entry(root, width = 35, borderwidth = 5)
-e.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 10)
-
-def num_(number_):
+def num_(number_, e_):
 	#e.dele
-	e.insert(END, number_) 
-def clear_():
+	#global END
+	e_.insert(END, number_) 
+def clear_(e_):
 	R_ = 0
-	e.delete(0, END)
+	e_.delete(0, END)
 
-def dot_():
-	e.insert(END, '.')
+def dot_(e_):
+	e_.insert(END, '.')
 
-def sum_():
+def add_(e_, Op_):
 	global R_
-	R_ = float(e.get())
+	R_ = float(e_.get())
 	global Operation_
-	Operation_ = 'Sum'
-	e.delete(0, END)
-def sus_():
+	Operation_ = Op_
+	e_.delete(0, END)
+def sus_(e_, Op_):
 	global R_
-	R_ = float(e.get())
+	R_ = float(e_.get())
 	global Operation_
-	Operation_ = 'Substraction'
-	e.delete(0, END)
-def mul_():
+	Operation_ = Op_
+	e_.delete(0, END)
+def mul_(e_, Op_):
 	global R_
-	R_ = float(e.get())
+	R_ = float(e_.get())
 	global Operation_
-	Operation_ = 'Multiplication'
-	e.delete(0, END)
-def div_():
+	Operation_ = Op_
+	e_.delete(0, END)
+def div_(e_, Op_):
 	global R_
-	R_ = float(e.get())
+	R_ = float(e_.get())
 	global Operation_
-	Operation_ = 'Division'
-	e.delete(0, END)
+	Operation_ = Op_
+	e_.delete(0, END)
 
-def eq_():
-	second = e.get()
-	e.delete(0, END)
-	if 	Operation_ == 'Sum':
-		e.insert(0, R_ + float(second))
+def eq_(e_):
+	second = e_.get()
+	e_.delete(0, END)
+	if 	Operation_ == 'Addition':
+		e_.insert(0, R_ + float(second))
 	elif 	Operation_ == 'Substraction':
-		e.insert(0, R_ - float(second))
+		e_.insert(0, R_ - float(second))
 	elif 	Operation_ == 'Multiplication':
-		e.insert(0, R_ * float(second))
+		e_.insert(0, R_ * float(second))
 	elif 	Operation_ == 'Division':
-		e.insert(0, R_ / float(second))
+		if float(second) != 0:
+			e_.insert(0, R_ / float(second))
+		elif float(second) == 0.0:
+			e_.insert(0, 'SYNTAX_ERROR')
 
-def act_():
+
+def act_(e_):
 	return
